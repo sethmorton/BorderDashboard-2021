005b6ddd668a928eab8270f2aca0f972aabeedbe
Seth Morton <sethmorton@Seths-MacBook-Air.local>
2021-05-25 22:16:50 -0700

    add percentage to pie chart

--- src/includes/graph-builder.js
+++ src/includes/graph-builder.js
@@ -22,6 +22,27 @@ export default class graphBuilder {
                 }]
             },
             options: {
+                tooltips: {
+                    callbacks: {
+                        title: function(tooltipItem, data) {
+                            return data.labels[tooltipItem[0].index];
+                          },
+                        label: function(tooltipItem, data) {
+                            //get the concerned dataset
+                            var dataset = data.datasets[tooltipItem.datasetIndex];
+                            //calculate the total of this data set
+                            var total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
+                              return previousValue + currentValue;
+                            });
+                            //get the current items value
+                            var currentValue = dataset.data[tooltipItem.index];
+                            //calculate the precentage based on the total and current item, also this does a rough rounding to give a whole number
+                            var percentage = Math.floor(((currentValue/total) * 100)+0.5);
+                      
+                            return percentage + "%";
+                          }
+                    }
+                  },
                 responsive: true,
                 maintainAspectRatio: false,
                 scales: {
@@ -131,15 +152,15 @@ export default class graphBuilder {
                       }
                     }
                   },
-                  responsive: true,
-                  maintainAspectRatio: false,
-                  scales: {
-                      yAxes: [{
-                          ticks: {
-                              beginAtZero: true
-                          }
-                      }]
-                  }
+                    responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
             }
             
         });
